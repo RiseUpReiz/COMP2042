cc323f66ee5e7198afcdf20b680b9832479eca31
RiseUpReiz <efyck4@nottingham.edu.my>
2024-11-20 18:07:44 +0800

    Added javafx media -Added background music

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
 
     opens com.example.demo to javafx.fxml;
     exports com.example.demo.controller;
+    opens com.example.demo.levels to javafx.fxml;
 }
