48abfaff7fa453330810a5402c3931aecb532647
RiseUpReiz <efyck4@nottingham.edu.my>
2024-12-02 15:31:07 +0800

    Added background music to game

--- src/main/java/com/example/demo/controller/Main.java
+++ src/main/java/com/example/demo/controller/Main.java
@@ -1,26 +1,34 @@
 package com.example.demo.controller;
 
-import java.lang.reflect.InvocationTargetException;
-
+import com.example.demo.levels.MainMenu;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	private static final int SCREEN_WIDTH = 1300;
-	private static final int SCREEN_HEIGHT = 750;
+	public static final int SCREEN_WIDTH = 1300;
+	public static final int SCREEN_HEIGHT = 750;
 	private static final String TITLE = "Sky Battle";
-	private Controller myController;
+	private static final String BGM_FILE_PATH = "/com/example/demo/audios/bgm.mp3";
+
+
+	public static int getScreenWidth() {
+		return SCREEN_WIDTH;
+	}
+
+	public static int getScreenHeight() {
+		return SCREEN_HEIGHT;
+	}
 
 	@Override
-	public void start(Stage stage) throws ClassNotFoundException, NoSuchMethodException, SecurityException,
-			InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	public void start(Stage stage) throws SecurityException, IllegalArgumentException {
 		stage.setTitle(TITLE);
 		stage.setResizable(false);
 		stage.setHeight(SCREEN_HEIGHT);
 		stage.setWidth(SCREEN_WIDTH);
-		myController = new Controller(stage);
-		myController.launchGame();
+		MusicManager.getInstance().playBackgroundMusic(BGM_FILE_PATH);
+		MainMenu mainMenu = new MainMenu(stage);
+		mainMenu.show();
 	}
 
 	public static void main(String[] args) {
