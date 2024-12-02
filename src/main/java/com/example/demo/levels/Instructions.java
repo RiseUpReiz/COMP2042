de2ecd1a7891abda5168182b2b21d52b47743e4f
RiseUpReiz <efyck4@nottingham.edu.my>
2024-12-02 15:27:01 +0800

    Finalise and complete Instructions.java

--- src/main/java/com/example/demo/levels/Instructions.java
+++ src/main/java/com/example/demo/levels/Instructions.java
@@ -4,6 +4,7 @@ import com.example.demo.controller.Controller;
 import com.example.demo.controller.Main;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
@@ -15,28 +16,13 @@ import javafx.geometry.Pos;
 public class Instructions extends LevelParent {
 
     private final Stage stage;
-    private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/start-background.png";
+    private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/start-background.jfif";
     double screenWidth = Main.getScreenWidth();
     double screenHeight = Main.getScreenHeight();
 
     public Instructions(double screenHeight, double screenWidth, Controller controller, Stage stage) {
         super(BACKGROUND_IMAGE_NAME, screenHeight, screenWidth, 0, controller, stage);
         this.stage = stage;
-    }
-
-    @Override
-    protected void checkIfGameOver() {
-        // Not applicable for the instructions screen
-    }
-
-    @Override
-    protected void initializeFriendlyUnits() {
-        // Not applicable for the instructions screen
-    }
-
-    @Override
-    protected void spawnEnemyUnits() {
-        // Not applicable for the instructions screen
     }
 
     @Override
@@ -50,13 +36,15 @@ public class Instructions extends LevelParent {
 
         // Instruction texts
         Text text1 = new Text("Objective: Defeat enemies and avoid projectiles");
-        Text text2 = new Text("Use arrow key '>' to move right\nUse arrow key '<' to move left\nSpacebar to shoot");
+        Text text2 = new Text("Use arrow key '>' to move right\nUse arrow key '<' to move left" +
+                                 "\nSpacebar to shoot\nPress 'Esc' to pause game");
+
         text1.setStyle("-fx-font-size: 20px; -fx-fill: black; -fx-font-weight: bold;");
         text2.setStyle("-fx-font-size: 18px; -fx-fill: black; -fx-font-weight: bold;");
 
         // Back button
         Button backButton = new Button("Back to Main Menu");
-        backButton.setOnAction(e -> backToMainMenu());
+        backButton.setOnAction(e -> goToMainMenu());
 
         // VBox for text and button
         VBox vbox = new VBox(20);
@@ -77,11 +65,29 @@ public class Instructions extends LevelParent {
         Pane layout = new Pane(stackPane);
         getRoot().getChildren().add(layout);
 
+        // Disable esc
+        instructionsScene.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+            if (event.getCode().toString().equals("ESCAPE")) {
+                event.consume(); // Ignore esc presses
+            }
+        });
+
         stage.show();
     }
 
-    private void backToMainMenu() {
+    private void goToMainMenu() {
         MainMenu mainMenu = new MainMenu(stage);
         mainMenu.show(); // Go back to the main menu
+    }
+    @Override
+    protected void checkIfGameOver() {
+    }
+
+    @Override
+    protected void initializeFriendlyUnits() {
+    }
+
+    @Override
+    protected void spawnEnemyUnits() {
     }
 }
